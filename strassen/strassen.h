a1b120e6390c974b58f3284b98a94e5a0d969c03
BIKOY ELIE <emmanuel.bikoy@facsciences-uy1.cm>
2022-10-01 07:23:07 +0100

    strassen error handling

--- strassen/strassen.h
+++ strassen/strassen.h
@@ -37,10 +37,10 @@ namespace stra {
     ){
         for(int i=0; i < n; i++){
             for(int j=0; j < n; j++){
-                if (strcomp(sign, "-") == 0){
+                if (sign =='-'){
                     c[i][j] = a[i][j] - b[i][j];
                 }
-                else if (strcomp(sign, "+") == 0){
+                else if (sign == '+'){
                     c[i][j] = a[i][j] + b[i][j];
                 }
             }
@@ -59,7 +59,7 @@ namespace stra {
                               e(nHalf, cols), f(nHalf, cols), g(nHalf, cols), h(nHalf, cols),
                               r(nHalf, cols), s(nHalf, cols), t(nHalf, cols), u(nHalf, cols),
                               p1(nHalf, cols), p2(nHalf, cols), p3(nHalf, cols), p4(nHalf, cols), p5(nHalf, cols), p6(nHalf, cols), p7(nHalf, cols),
-                              FminusH(nHalf, cols), AplusB(nHalf, cols), GminusE(nHalf, cols), CplusD(nHalf, cols), AplusD(nHalf, cols), EminusH(nHalf, cols), BminusD(nHalf, cols), GplusH(nHalf, cols), EplusF(nHalf, cols), AminusC(nHalf, cols),
+                              FminusH(nHalf, cols), AplusB(nHalf, cols), GminusE(nHalf, cols), CplusD(nHalf, cols), AplusD(nHalf, cols), EplusH(nHalf, cols), BminusD(nHalf, cols), GplusH(nHalf, cols), EplusF(nHalf, cols), AminusC(nHalf, cols),
                               P5plusP4(nHalf, cols), P6minusP2(nHalf, cols), P5minusP3(nHalf, cols), P1minusP7(nHalf, cols);
 
 
@@ -71,52 +71,52 @@ namespace stra {
         divideMatrix(e, f, g, h, B, n-1);
 
         //determine p1
-        subtractOrSum(f, h, FminusH, "-", nHalf);
+        subtractOrSum(f, h, FminusH, '-', nHalf);
         strassen(a, FminusH, p1, nHalf);
 
         //determine p2
-        subtractOrSum(a, b, AplusB, "+", nHalf);
+        subtractOrSum(a, b, AplusB, '+', nHalf);
         strassen(AplusB, h, p2, nHalf);
 
         //determine p3
-        subtractOrSum(c, d, CplusD, "+", nHalf);
+        subtractOrSum(c, d, CplusD, '+', nHalf);
         strassen(CplusD, e, p3, nHalf);
 
         //determine p4
-        subtractOrSum(g, e, GminusE, "-", nHalf);
+        subtractOrSum(g, e, GminusE, '-', nHalf);
         strassen(d, GminusE, p4, nHalf);
 
         //determine p5
-        subtractOrSum(a, d, AplusD, "+", nHalf);
-        subtractOrSum(e, h, EplusH, "+", nHalf);
+        subtractOrSum(a, d, AplusD, '+', nHalf);
+        subtractOrSum(e, h, EplusH, '+', nHalf);
         strassen(AplusD, EplusH, p4, nHalf);
 
         //determine p6
-        subtractOrSum(b, d, BminusD, "-", nHalf);
-        subtractOrSum(g, h, GplusH, "+", nHalf);
+        subtractOrSum(b, d, BminusD, '-', nHalf);
+        subtractOrSum(g, h, GplusH, '+', nHalf);
         strassen(BminusD, GplusH, p6, nHalf);
 
         //determine p7
-        subtractOrSum(a, c, AminusC, "-", nHalf);
-        subtractOrSum(e, f, EplusF, "+", nHalf);
+        subtractOrSum(a, c, AminusC, '-', nHalf);
+        subtractOrSum(e, f, EplusF, '+', nHalf);
         strassen(AminusC, EplusF, p1, nHalf);
 
 
         // calculate r
-        subtractOrSum(p6, p2, P6minusP2, "-", nHalf);
-        subtractOrSum(p5, p4, P5plusP4, "+", nHalf);
-        subtractOrSum(P6minusP2, P5plusP4, r, "+", nHalf);
+        subtractOrSum(p6, p2, P6minusP2, '-', nHalf);
+        subtractOrSum(p5, p4, P5plusP4, '+', nHalf);
+        subtractOrSum(P6minusP2, P5plusP4, r, '+', nHalf);
 
         // calculate u
-        subtractOrSum(p5, p3, P5minusP3, "-", nHalf);
-        subtractOrSum(p1, p7, P1minusP7, "-", nHalf);
-        subtractOrSum(P5minusP3, P1minusP7, u, "+", nHalf);
+        subtractOrSum(p5, p3, P5minusP3, '-', nHalf);
+        subtractOrSum(p1, p7, P1minusP7, '-', nHalf);
+        subtractOrSum(P5minusP3, P1minusP7, u, '+', nHalf);
 
         // caculate s
-        subtractOrSum(p1, p2, s, "+", nHalf);
+        subtractOrSum(p1, p2, s, '+', nHalf);
 
         // calculate t
-        subtractOrSum(p3, p4, t, "+", nHalf);
+        subtractOrSum(p3, p4, t, '+', nHalf);
         
 
         for(int i=0; i < nHalf; i++){
